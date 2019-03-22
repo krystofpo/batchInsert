ca70bd21c8a5397adbb849afb9875b56cf5b0bf8
krystof.polansky <krystof.polansky@rohlik.cz>
2019-03-22 20:36:38 +0100

    10. test for borrowing ok , PASS, test for returning ok, PASS
    REST endpoints test s pujcenim a vracenim knih ok
    
    reposiroty nahrazeny querydsl, ok
    
    liquibase funguje, pred cistenim
    
    springboot runs ok

--- src/main/resources/schema_init_scripts.sql
+++ src/main/resources/schema_init_scripts.sql
@@ -1,15 +1,14 @@
 
-    drop table Book if exists
+drop table Book if exists;
 
 
-    drop table Loan if exists
+drop table Loan if exists;
 
 
-    drop table User if exists
+drop table User if exists;
 
-
-    drop sequence if exists id_seq
-create sequence id_seq start with 1 increment by 50
+drop sequence if exists id_seq;
+create sequence id_seq;
 
 
     create table Book (
@@ -17,14 +16,14 @@ create sequence id_seq start with 1 increment by 50
         name varchar(255),
         loan_id bigint,
         primary key (id)
-    )
+    );
 
     create table Loan (
        id bigint not null,
         name varchar(255),
         user_id bigint,
         primary key (id)
-    )
+    );
 
 
     create table User (
@@ -32,16 +31,16 @@ create sequence id_seq start with 1 increment by 50
         hasLoans boolean not null,
         name varchar(255),
         primary key (id)
-    )
+    );
 
 
     alter table Book
        add constraint "FK_BOOK_LOAN"
        foreign key (loan_id)
-       references Loan
+       references Loan;
 
 
     alter table Loan
        add constraint "FK_LOAN_USER"
        foreign key (user_id)
-       references User
+       references User;
